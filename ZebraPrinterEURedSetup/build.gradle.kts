279996970afc3a21914969ce696aecaa299ea27d
Trudu Laurent <laurent@trudu.com>
2026-01-19 17:21:38 +0100

    Update version number

--- ZebraPrinterEURedSetup/build.gradle.kts
+++ ZebraPrinterEURedSetup/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zebra.zebraprintereuredsetup.dev"
         minSdk = 29
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.1.0"
+        versionCode = 15
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
